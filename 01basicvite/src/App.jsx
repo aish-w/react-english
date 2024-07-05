fc4d00b848872734ea01d6170aeb89e868cd7730
aishwarya <aishwaryamaheshwari02@gmail.com>
2024-07-05 20:16:13 +0530

    modify changes in the code

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+
+function App() {
+  const username="aishwarya"
+  return (
+    <>
+   <h1>vite react app {2+2}</h1>
+   <h1>vite react app {username}</h1>
+   <Youtube/>
+   </>
+  )
+}
+
+export default App
